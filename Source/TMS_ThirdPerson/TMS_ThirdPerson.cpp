83245c662f983110d84daee2578ccea9ff035363
Aliaksei Ramanovich <aliaksie.ramanovich@vizor-games.com>
2023-02-18 14:45:18 +0100

    Add project

---
+++ Source/TMS_ThirdPerson/TMS_ThirdPerson.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TMS_ThirdPerson.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TMS_ThirdPerson, "TMS_ThirdPerson" );
